64ab7e2029e6a95607c21dbb971f2ca03bf2ca37
KGD <119135727+r3allytalltruth@users.noreply.github.com>
2022-11-27 13:24:52 -0800

    Verification failed in GitHub, loading here
    deeks@realtool MINGW64 /c/R_Git_stuff/ProgrammingAssignment2 (master)
    $ git push
    remote: Permission to rdpeng/ProgrammingAssignment2.git denied to r3allytalltruth.
    fatal: unable to access 'https://github.com/rdpeng/ProgrammingAssignment2/': The requested URL returned error: 403

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+### R Programming Assignment2: Caching the Inverse of a Matrix
+## Hi and HELLO- Comments will show my work
+# First I make a simple matrix, called matrix1 with a simple matrix inverse n1
+## matrix1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+## when called, matrix1
+##        [,1]  [,2]
+##  [1,]  0.50 -1.00
+##  [2,] -0.25  0.75
+## matrix2 <- makeCacheMatrix(matrix1)
+## cacheSolve(matrix2)
 
-## Write a short comment describing this function
+# PT1 `makeCacheMatrix`function creates a special "matrix" object, can invert,
+# then call > makeCacheMatrix(matrix1)
 
 makeCacheMatrix <- function(x = matrix()) {
+  # cached inverse of the matrix
+  inv <- NULL
+  get <- function() x
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  ## get and set for the matrix inverse
+  getinv <- function() inv
+  setinv <- function(inverse) inv <<- inverse
+  
+  ## make list of functions for the matrix
+  list(set = set, get = get,
+       getinv = getinv,
+       setinv = setinv)
 
 }
 
 
-## Write a short comment describing this function
+
+## PT2 `cacheSolve`computes the inverse of the special
+##"matrix" returned by `makeCacheMatrix` above. If the inverse has
+##already been calculated (and the matrix has not changed), then
+##`cacheSolve` should retrieve the inverse from the cache.
+## matrix2 <- makeCacheMatrix(matrix1)
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    ## makes cache matrix (if already computed)
+    if (!is.null(inv)) {
+      message("inverse is cached")
+      return(inv)
+    }
+    #computes the inverse of the matrix
+    m <- x$get()
+    inv <- solve(m, ...)
+    
+    #cache inverse
+    x$setinv(inv)
+    
+    #returns that inverse of the matrix
+    return(inv)
 }
+
+
+
